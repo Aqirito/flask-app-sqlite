0cb3a8305f871157416d05f25c40f88d3363332d
Aqirito <aqil.rahmat99@gmail.com>
2023-05-20 11:54:43 +0800

    UPDATE: remove cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 import os
 
 from flask import Flask
-from flask_cors import CORS
 
 
 def create_app(test_config=None):
@@ -11,7 +10,6 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
     )
-    CORS(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
